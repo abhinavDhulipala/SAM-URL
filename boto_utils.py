0aaf7cfd6a988a19dd7d3740a6f9658bfa50f4fe
abhinavDhulipala <46908860+abhinavDhulipala@users.noreply.github.com>
2021-03-14 18:45:30 -0700

    created basic SAM app, runtime failures aplenty

--- boto_utils.py
+++ boto_utils.py
@@ -1,6 +1,7 @@
 import boto3 as b3
 from local_constants import AWS_PROFILE
 from constants import TABLE_NAME
+import datetime
 
 flask_profile = b3.session.Session(profile_name=AWS_PROFILE)
 dynamodb = flask_profile.resource('dynamodb')
@@ -50,6 +51,33 @@ def migration(t_name=table_name):
     )
 
     return created_table
+
+"""
+This method will execute put requests to dynamo URL table.
+@:param original_url -> string of original url
+@:param redirect_url -> string of the new url
+@:param expiration_date -> string of the expiration date of the redirect url
+@:param user -> string of user id
+@:param table_name -> string of name of table to be queried (defaults to TABLE_NAME in constants.py)
+@:returns -> response of put request if table exists; returns False otherwise
+"""
+def put(original_url, redirect_url, expiration_date, user, table_name=TABLE_NAME):
+    if not table_exists(table_name):
+        print("That table does not exist!")
+        return False
+
+    table = dynamodb.Table(table_name)
+    response = table.put_item(
+        Item={
+            'redirect_url': redirect_url,
+            'original_url': original_url,
+            'creation_date': datetime.now().strftime('%s'),
+            'expiration_date': expiration_date,
+            'user': user
+        }
+    )
+
+    return response
 
 
 if __name__ == '__main__':
